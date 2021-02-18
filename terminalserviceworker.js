e9cf487f19e49343b647ffb520dc26d9cd7a9f6a
lukasozpaker <44107295+lukasozpaker@users.noreply.github.com>
2021-02-18 16:46:46 -0500

    Add files via upload

--- terminalserviceworker.js
+++ terminalserviceworker.js
@@ -1,6 +1,6 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
 
-workbox.routing.registerRoute(
-    ({request}) => request.destination === "image",
-    new workbox.strategies.CacheFirst()
-);
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination === "text",
+//     new workbox.strategies.CacheFirst()
+// );
