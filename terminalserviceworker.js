10247eae6196a39d7f1566b6347119dca16264e5
lukasozpaker <44107295+lukasozpaker@users.noreply.github.com>
2021-02-19 15:41:55 -0500

    Delete terminalserviceworker.js

--- terminalserviceworker.js
+++
@@ -1,6 +0,0 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
-
-// workbox.routing.registerRoute(
-//     ({request}) => request.destination === "text",
-//     new workbox.strategies.CacheFirst()
-// );
