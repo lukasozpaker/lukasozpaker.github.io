8a4b23077f9d069c377a3408e6f084f52a01de37
lukasozpaker <44107295+lukasozpaker@users.noreply.github.com>
2021-02-19 21:35:51 -0500

    Add files via upload

--- terminalsw.js
+++ terminalsw.js
@@ -1,7 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
 
 
-const staticCache = 'site-static-v467631556';
+const staticCache = 'site-static-8767458786';
 // const dynamicCache = 'site-dynamic-v1';
 
 
@@ -11,7 +11,8 @@ const assets = [
     "terminalmanifest.json",
     "terminal/terminal512.png",
     "https://fonts.googleapis.com/icon?family=Material+Icons",
-    "https://fonts.gstatic.com/s/materialicons/v77/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+    "https://fonts.gstatic.com/s/materialicons/v77/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+    "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
 ];
 
 
@@ -42,7 +43,7 @@ self.addEventListener("activate", e => {
                 .filter(key => key !== staticCache)
                 .map(key => caches.delete(key))
             )
-        })
+        }) 
     );
 });
 
@@ -51,19 +52,6 @@ self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(cacheRes => {
             return cacheRes || fetch(e.request);
-            /*
-            //.then() for dynamic caching (not really necessary for terminal 1.2 but might be useful in the future)
-
-
-            .then(fetchRes => {
-                //take data returned from fetch and put it in cache
-                return caches.open(dynamicCache).then(cache => {
-                    //clone() because can only use fetchRes once
-                    cache.put(e.request.url, fetchRes.clone());
-                    return fetchRes;
-                })
-            });
-            */
         })
     );
 });
