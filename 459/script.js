06f0f9a288662c711ca9bd202f79b2d15f43fd1c
Lukas Ozpaker <44107295+lukasozpaker@users.noreply.github.com>
2024-05-17 12:33:46 -0400

    edit 2

--- 459/script.js
+++ 459/script.js
@@ -1,4 +1,4 @@
-alert("Version 1");
+alert("Version 2");
 
 const canvas = document.createElement('canvas');
 canvas.width = 300;
@@ -9,9 +9,16 @@ const ctx = canvas.getContext('2d');
 
 function drawGraph(ls) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   ctx.beginPath();
+  if(ls[0] == "OUT OF RANGE"){
+    ctx.moveTo(0, canvas.height - 0);
+  }
   ctx.moveTo(0, canvas.height - ls[0] * 10);
   for (let i = 1; i < ls.length; i++) {
+    if(ls[i] == "OUT OF RANGE"){
+      ctx.lineTo(i * 10, canvas.height - 0);
+    }
     ctx.lineTo(i * 10, canvas.height - ls[i] * 10);
   }
   ctx.stroke();
