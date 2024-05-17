4805dbf8cc15d940c94e633a184c55ca62dcf762
Lukas Ozpaker <44107295+lukasozpaker@users.noreply.github.com>
2024-05-17 12:47:00 -0400

    edit

--- 459/script.js
+++ 459/script.js
@@ -8,18 +8,34 @@ document.body.appendChild(canvas);
 const ctx = canvas.getContext('2d');
 
 function drawGraph(ls) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.beginPath();
-  if(ls[0] == "OUT OF RANGE"){
-    ctx.moveTo(0, canvas.height - 0);
-  }
-  ctx.moveTo(0, canvas.height - ls[0] * 10);
-  for (let i = 1; i < ls.length; i++) {
-    if(ls[i] == "OUT OF RANGE"){
-      ctx.lineTo(i * 10, canvas.height - 0);
+    const min = Math.min(...ls);
+    const max = Math.max(...ls);
+
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height);
+    for (let i = 0; i < ls.length; i++) {
+        const x = i / (ls.length - 1) * canvas.width;
+        const y = (1 - (ls[i] - min) / (max - min)) * canvas.height;
+        ctx.lineTo(x, y);
     }
-    ctx.lineTo(i * 10, canvas.height - ls[i] * 10);
-  }
-  ctx.stroke();
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.closePath();
+    ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(canvas.width, 0);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.lineTo(0, canvas.height);
+    ctx.closePath();
+    ctx.stroke();
+
+
+    ctx.font = '20px Arial';
+
+    ctx.fillText(new Date().toLocaleTimeString(), 0, 40);
+
 }
